291a8c4b2c2fea6ce9905e8f6d3f848a5db95bfc
krishnavenimandula <krishnavenimandula123@gmail.com>
2025-04-17 18:03:12 +0100

    removed unnecessary variable

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { Link, useLocation } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 
 const navLinks = [
   { name: "Home", path: "#home" },
